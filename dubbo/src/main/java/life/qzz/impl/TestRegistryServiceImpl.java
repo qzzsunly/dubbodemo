4ce79a45633fe702a098b4ed9c559022a5121422
Q ZZ <Q ZZ>
2017-04-07 18:51:55 +0800

    init

---
+++ dubbo/src/main/java/life/qzz/impl/TestRegistryServiceImpl.java
@@ -0,0 +1,9 @@
+package life.qzz.impl;
+
+import org.springframework.stereotype.Service;
+@Service("testRegistryService")
+public class TestRegistryServiceImpl implements TestRegistryService{
+    public String hello(String name) {
+        return "hello" + name;
+    }
+}
