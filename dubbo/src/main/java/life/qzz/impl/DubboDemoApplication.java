b9f0bc56d3455b06c306898773cbfb75de3a8997
qzz <qzzsunly@163.com>
2017-04-09 23:38:01 +0800

    dubbo.xml

--- dubbo/src/main/java/life/qzz/impl/DubboDemoApplication.java
+++ dubbo/src/main/java/life/qzz/impl/DubboDemoApplication.java
@@ -1,9 +1,13 @@
 package life.qzz.impl;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ImportResource("dubbo.xml")
 public class DubboDemoApplication {
 
 	public static void main(String[] args) {
