41474f9a83602f27564393e5bbc2e231fcfaf8d8
qzz <qzzsunly@163.com>
2017-04-20 11:51:23 +0800

    dubbo.xml

--- consumer/src/main/java/life/qzz/impl/TestRegistryServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package life.qzz.impl;
-
-import org.springframework.stereotype.Service;
-@Service("testRegistryService")
-public class TestRegistryServiceImpl implements TestRegistryService{
-    public String hello(String name) {
-        return "hello" + name;
-    }
-}
